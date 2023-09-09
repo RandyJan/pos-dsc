f0a57864dd4d733915367ce257679f2295e62752
dev-reymark <dev.reymark@gmail.com>
2023-09-09 11:17:19 +0800

    Resize logo for nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href=" ">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <x-nav-logo style="width: 50px; height: 40px;" class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
